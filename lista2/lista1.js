a13e7185411887feac50d6c98c74d8d59528d528
macielbarbosa <macielbarbosa@outlook.com>
2017-08-30 15:59:30 -0300

    Add files via upload

---
+++ lista2/lista1.js
@@ -0,0 +1,5 @@
+function mudar(){
+	for (var i = 1; i<=10; i++) {
+		alert('mudou '+i);
+	}
+}
---
+++ lista2/script.js
@@ -0,0 +1,7 @@
+function ligar(){
+	document.getElementById('lampada').src = 'Lon.png'
+}
+
+function desligar(){
+	document.getElementById('lampada').src = 'Loff.png'
+}
