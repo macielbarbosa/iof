a02ed7e5cf9432a364e5a84092feb0ed7a57bcad
macielbarbosa <macielbarbosa@outlook.com>
2017-09-12 18:21:40 -0300

    Add files via upload

--- lista2/script.js
+++ lista2/script.js
@@ -1,7 +1,9 @@
 function ligar(){
-	document.getElementById('lampada').src = 'Lon.png'
+	document.getElementById('lampada').src = 'Lon.png';
+	document.body.style.backgroundColor = 'white';
 }
 
 function desligar(){
-	document.getElementById('lampada').src = 'Loff.png'
+	document.getElementById('lampada').src = 'Loff.png';
+	document.body.style.backgroundColor = 'gray';
 }
