6720bb1ded5b1c69553c0e86068afd0a8508681a
macielbarbosa <macielbarbosa@outlook.com>
2017-09-27 16:41:46 -0300

    Add files via upload

--- lista3/server.php
+++ lista3/server.php
@@ -8,11 +8,16 @@
 	<br><br>
 	<h2>Seus dados foram validados com sucesso!</h2>
 	<br>
-	<?php 
-		$conexao;
+	<?php
+		$conexao; $nome; $sobrenome; $cpf; $idade; $logado=false;
+		$usuario = $_POST['login'];
+		$senha = $_POST['senha'];
+		
 		conectar('localhost','id1996929_macielbarbosa','mbs1994','id1996929_iot_db');
-		login($_POST['login'],$_POST['senha']);
-        
+		login($usuario,$senha);
+		if($logado==true)
+			mostrarDados($nome,$sobrenome,$cpf,$idade);
+
         function conectar($servidor,$usuario,$senha,$banco){
         	global $conexao;
         	$conexao = new mysqli($servidor, $usuario, $senha, $banco);
@@ -25,18 +30,30 @@
         }
 
         function login($usuario,$senha){
-			global $conexao;
-        	$consulta = "SELECT nome, sobrenome FROM usuarios WHERE usuario='".$usuario."' AND senha='".$senha."'";
+			global $conexao, $nome, $sobrenome, $cpf, $idade, $logado;
+        	$consulta = "SELECT nome, sobrenome, cpf, idade FROM usuarios WHERE usuario='".$usuario."' AND senha='".$senha."'";
 			$resultado = $conexao->query($consulta);
 			if($resultado->num_rows > 0){
+				$logado = true;
 				while ($linha = $resultado->fetch_assoc()) {
-				    echo "<h3>Seja bem vindo ".$linha['nome']." ".$linha['sobrenome']."<h3>";
+				    echo "<h3>Seja bem vindo ".$linha['nome']." ".$linha['sobrenome']."</h3>";
+				    $nome = $linha['nome'];
+				    $sobrenome = $linha['sobrenome'];
+				    $cpf = $linha['cpf'];
+				    $idade = $linha['idade'];
 				}
 			}
 			else{
 				echo "Usuário ou senha incorreta";
 			}
 			$conexao->close();
+		}
+
+		function mostrarDados($nome,$sobrenome,$cpf,$idade){
+			echo "<br><br>Nome: ".$nome;
+			echo "<br><br>Sobrenome: ".$sobrenome;
+			echo "<br><br>CPF: ".$cpf;
+			echo "<br><br>Idade: ".$idade;
 		}
 	 ?>
 </body>
